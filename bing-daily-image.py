bfbc2b61f436c10082666d93c439ca0ecb2fd858
sebasijan <sebastian.otto.kotze@gmail.com>
2021-12-25 19:34:29 +0200

    .

--- bing-daily-image.py
+++ bing-daily-image.py
@@ -1,25 +1,46 @@
 # get the daily image
 # store it in firefox customisation folder
+# store it in my wallpapers/bing folder
 # see 
-#   userContent.css in same folder
+#   userContent.css in same firefox folder
 #   crontab -e
 
 import requests
 
-image_info_url = 'https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-UK'
-destination_file = 'home-background.jpg'
-firefox_folder = '/home/bassie/.mozilla/firefox/'
-release_folder = firefox_folder + 'luneagrw' + '.default-release-1/'
-destination_folder = release_folder + 'chrome/img/'
-destination_path = destination_folder + destination_file 
+class ImageData: 
+    def __init__(self, data, title):
+        self.data = data
+        self.title = title
 
-response = requests.get(image_info_url).json()
-image_info = response['images'][0]
-image_path = image_info['url']
-# image_title = image_info['title']
+def get_firefox_destination_path():
+    firefox_folder = '/home/bassie/.mozilla/firefox/'
+    firefox_release_folder = firefox_folder + 'luneagrw' + '.default-release-1/'
+    destination_folder = firefox_release_folder + 'chrome/img/'
+    destination_file = 'home-background.jpg'
+    destination_path = destination_folder + destination_file
+    return destination_path
 
-image_url = 'https://bing.com' + image_path
-image_data = requests.get(image_url).content
+def get_image_data(): 
+    image_info_url = 'https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-UK'
+    response = requests.get(image_info_url).json()
+    image_info = response['images'][0]
+    print(image_info)
+    image_path = image_info['url']
+    image_name = image_info['copyright'].split("(")[0] # trying to exclude copyright symbol
+    image_url = 'https://bing.com' + image_path
+    image_data = requests.get(image_url).content
+    return ImageData(image_data, image_name)
 
-with open(destination_path, 'wb') as handler:
-    handler.write(image_data)
+def save_firefox_image(image_data):
+    firefox_image_path = get_firefox_destination_path()
+    with open(firefox_image_path, 'wb') as handler:
+        handler.write(image_data)
+
+def save_wallpaper_image(image_data, title):
+    with open('/home/bassie/Pictures/wallpapers/bing/'+ title + '.jpg', 'wb') as handler:
+        handler.write(image_data)
+
+image_data = get_image_data()
+
+save_firefox_image(image_data.data)
+save_wallpaper_image(image_data.data, image_data.title)
