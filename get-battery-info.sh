312c9eeea7aec97b08dc6c235de5f7f57c17e140
sebasijan <sebastian.otto.kotze@gmail.com>
2022-01-03 18:20:31 +0200

    .

--- get-battery-info.sh
+++ get-battery-info.sh
@@ -4,25 +4,70 @@ status=`cat /sys/class/power_supply/BAT0/status`
 capacity=`cat /sys/class/power_supply/BAT0/capacity`
 
 if [ "$status" == "Full" ]; then
-    outy="████"
+    echo ""
+    exit 0
 else 
-    if [ "$status" == "Discharging" ]; then
-        outy="▒"
-        symbol="▒"
-    else
-        outy="█"
-        symbol="█"
+    if [ "$capacity" -gt 80 ]; then
+        colour="#00ff00"
+        battery="█"
+    elif [ "$capacity" -gt 70 ]; then
+        colour="#7de800"
+        battery="▇"
+    elif [ "$capacity" -gt 60 ]; then
+        colour="#aad000"
+        battery="▆"
+    elif [ "$capacity" -gt 50 ]; then
+        colour="#cab500"
+        battery="▅"
+    elif [ "$capacity" -gt 40 ]; then
+        colour="#e29700"
+        battery="▄"
+    elif [ "$capacity" -gt 30 ]; then
+        colour="#f27600"
+        battery="▃"
+    elif [ "$capacity" -gt 20 ]; then
+        colour="#fc4f00"
+        battery="▂"
+    elif [ "$capacity" -gt 10 ]; then
+        colour="#ff0000"
+        battery="▁"
     fi
-
-    for ((i=25;i<="$capacity";i=i+25)); do
-        outy="${outy}${symbol}"
-    done
-
-    for ((i="$capacity";i<100;i=i+25)); do
-        outy="${outy}"
-    done
+    
+    echo "<fc=${colour}>${battery}</fc>"
 fi
 
-echo "${outy}■"
+
+# ▁
+# ▂
+# 
+# ▄
+# 
+# ▆
+# ░
+# █ 
+
+# if [ "$status" == "Full" ]; then
+#     # outy="████"
+#     echo ""
+#     exit 0
+# else 
+#     if [ "$status" == "Discharging" ]; then
+#         outy="▒"
+#         symbol="▒"
+#     else
+#         outy="█"
+#         symbol="█"
+#     fi
+
+#     for ((i=25;i<="$capacity";i=i+25)); do
+#         outy="${outy}${symbol}"
+#     done
+
+#     for ((i="$capacity";i<100;i=i+25)); do
+#         outy="${outy}"
+#     done
+# fi
+
+# echo "${outy}■"
 
 exit 0
--- get-volume.sh
+++ get-volume.sh
@@ -4,19 +4,56 @@ mutedStatus=`amixer -c 0 -D pulse get Master | tail -n 1 | grep -c '\[on\]'`
 volume=`amixer -D pulse get Master | awk -F'[]%[]' '/%/ {if ($7 == "off") { print "MM" } else { print $2 }}' | head -n 1`
 
 if [ "$mutedStatus" -eq 0 ]; then
-    symbol="░"
+    colour="#333333"
 else 
-    symbol="█"
+    colour="white"
 fi
 
-for ((i=10;i<="$volume";i=i+10)); do
-    outy="${outy}${symbol}"
-done
+full="┃"
+half="╽"
+empty="┊"
 
-for ((i="$volume";i<100;i=i+10)); do
-    outy="${outy}■"
-done
+case "$volume" in
+    0)
+        echo "<fc=${colour}>${empty}${empty}${empty}${empty}${empty}</fc>";;
+    10)
+        echo "<fc=${colour}>${half}${empty}${empty}${empty}${empty}</fc>";;   
+    20)
+        echo "<fc=${colour}>${full}${empty}${empty}${empty}${empty}</fc>";;
+    30)
+        echo "<fc=${colour}>${full}${half}${empty}${empty}${empty}</fc>";;
+    40)
+        echo "<fc=${colour}>${full}${full}${empty}${empty}${empty}</fc>";;
+    50)
+        echo "<fc=${colour}>${full}${full}${half}${empty}${empty}</fc>";;   
+    60)
+        echo "<fc=${colour}>${full}${full}${full}${empty}${empty}</fc>";;
+    70)
+        echo "<fc=${colour}>${full}${full}${full}${half}${empty}</fc>";;
+    80)
+        echo "<fc=${colour}>${full}${full}${full}${full}${empty}</fc>";;
+    90)
+        echo "<fc=${colour}>${full}${full}${full}${full}${half}</fc>";;
+    100)
+        echo "<fc=${colour}>${full}${full}${full}${full}${full}</fc>";;
+esac
 
-echo "$outy"
+# ${half}${full}┊${empty}
+
+# if [ "$mutedStatus" -eq 0 ]; then
+#     symbol="░"
+# else 
+#     symbol="█"
+# fi
+
+# for ((i=10;i<="$volume";i=i+10)); do
+#     outy="${outy}${symbol}"
+# done
+
+# for ((i="$volume";i<100;i=i+10)); do
+#     outy="${outy}■"
+# done
+
+# echo "$outy"
 
 exit 0
